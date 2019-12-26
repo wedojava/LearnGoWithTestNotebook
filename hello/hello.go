a043d45d05243829d869eb81446656085668614e
wedojava <wedojava@gmail.com>
2019-12-26 17:31:52 +0800

    write assert part.

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+const englishHelloSuffix = "!"
+
 func main() {
 	fmt.Println(Hello("world"))
 }
 
 // Hello just a begin of Go programing
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name + englishHelloSuffix
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("hans")
-	want := "Hello, hans!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCurrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("hans")
+		want := "Hello, hans!"
+		assertCurrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world!' when an empty string is supplied.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCurrectMessage(t, got, want)
+	})
 }
