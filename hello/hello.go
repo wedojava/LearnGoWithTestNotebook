881f083b463d04e6afeac94d67fb928514e0d1d8
wedojava <wedojava@gmail.com>
2019-12-26 19:00:28 +0800

    final version of hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const chinese = "Chinese"
 const englishHelloPrefix = "Hello, "
-const englishHelloSuffix = "!"
-
-func main() {
-	fmt.Println(Hello("world"))
-}
+const spanishHelloPrefix = "Hola, "
+const chineseHelloPrefix = "你好, "
+const suffix = "!"
 
 // Hello just a begin of Go programing
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name + englishHelloSuffix
+
+	return greetingPrefix(language) + name + suffix
+}
+
+// 公共函数( 例如上面的 Hello() )以大写字母开始，私有函数以小写字母开头。
+func greetingPrefix(language string) (prefix string) { // 命名返回值 (prefix string) 将在函数中创建一个名为 prefix 的变量
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case chinese:
+		prefix = chineseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return // 只通过调用 return 而不是 return prefix 来返回值。
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("hans")
+		got := Hello("hans", "")
 		want := "Hello, hans!"
 		assertCurrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world!' when an empty string is supplied.", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world!"
+		assertCurrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+		assertCurrectMessage(t, got, want)
+	})
+
+	t.Run("in Chinese", func(t *testing.T) {
+		got := Hello("龙哥", "Chinese")
+		want := "你好, 龙哥!"
 		assertCurrectMessage(t, got, want)
 	})
 }
