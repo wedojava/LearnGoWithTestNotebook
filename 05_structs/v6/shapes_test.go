15097e5e5f9124ec0222fe47f870a1b3222891e0
wedojava <wedojava@gmail.com>
2020-01-06 01:49:38 +0800

    add step 6

---
+++ 05_structs/v6/shapes_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+	// https://learnku.com/docs/learn-go-with-tests/structure-method-and-interface/6084#129bda
+}
