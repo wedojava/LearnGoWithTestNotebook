4008a18a884f54a5e5eb6c082a54218c78c44ad9
wedojava <wedojava@gmail.com>
2020-01-21 00:41:27 +0800

    使用 ErrNotFound 变量

--- 07_dictionary/dictionary_test.go
+++ 07_dictionary/dictionary_test.go
@@ -12,15 +12,21 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("expected to get an error.")
+	}
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertStrings(t *testing.T, got, want string) {
