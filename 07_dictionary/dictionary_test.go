9903ae9c1b0131579ee59232c0ffa23046475f66
wedojava <wedojava@gmail.com>
2020-01-21 00:49:59 +0800

    just pass the test

--- 07_dictionary/dictionary_test.go
+++ 07_dictionary/dictionary_test.go
@@ -18,6 +18,21 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should not find added word: ", err)
+	}
+
+	if want != got{
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
 
