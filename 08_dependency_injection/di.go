482249950852b5cf29ec790d62b0283c876ab849
wedojava <wedojava@gmail.com>
2020-01-22 14:45:44 +0800

    print hello world to website

--- 08_dependency_injection/di.go
+++ 08_dependency_injection/di.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+
+func MyGreeterHandle(w http.ResponseWriter, r *http.Request)  {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandle))
 }
